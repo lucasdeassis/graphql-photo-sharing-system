945765df005f10faed8645e9e9a71f6607d1fd51
lucasdeassis <lucasassis413@gmail.com>
2018-09-09 21:19:58 -0300

    include user model to login and register in auth resolver

--- server/graphql/types/auth/resolver.js
+++ server/graphql/types/auth/resolver.js
@@ -1,17 +1,15 @@
+const User = require('../../../models/user');
+
 module.exports = {
-    resolver: {
-        Mutation: {
-            register: async (root, args, context) => {
-                // TODO: Handle user registration
-            },
+  resolver: {
+    Mutation: {
+      register: async (root, { username, password }, { db }) => new User().register(db, username, password),
 
-            login: async (root, args, context) => {
-                // TODO: Handle user login here
-            },
+      login: async (root, { username, password }, { db }) => new User().login(db, username, password),
 
-            logout: async (root, args, context) => {
-                // TODO: Handle user logout here
-            },
-        },
+      logout: async (root, args, context) => {
+        // TODO: Handle user logout here
+      },
     },
+  },
 };
