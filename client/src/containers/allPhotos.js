74da9e8270a4d491d18887d29310d995161923d2
lucasdeassis <lucasassis413@gmail.com>
2018-09-17 04:05:49 -0300

    implement subscribeToMore along with photos query

--- client/src/containers/allPhotos.js
+++ client/src/containers/allPhotos.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import { PhotoList } from '../components/PhotoList';
 
 export const GET_ALL_PHOTOS = gql`
     query getAllPhotos {
@@ -13,10 +13,46 @@ export const GET_ALL_PHOTOS = gql`
     }
 `;
 
-const AllPhotos = ({ children }) => <Query query={GET_ALL_PHOTOS}>{children}</Query>;
+export const PHOTO_SUBSCRIPTION = trigger => gql`
+  subscription {
+    ${trigger} {
+      id
+      width,
+      height,
+    }
+  }
+`;
 
-AllPhotos.propTypes = {
-    children: PropTypes.func.isRequired,
-};
+const AllPhotos = () => (
+  <Query query={GET_ALL_PHOTOS}>
+    {
+      ({
+        loading, error, data, subscribeToMore,
+      }) => {
+        if (loading) return <div>Loading...</div>;
+        if (error) return <div>Error :(</div>;
+
+        return (
+          <PhotoList
+            data={data}
+            subscribeToNewPhotos={() => subscribeToMore({
+              document: PHOTO_SUBSCRIPTION('photoDeleted'),
+              updateQuery: (prev, { subscriptionData }) => {
+                if (!subscriptionData.data) return prev;
+
+                return {
+                  photos: [
+                    ...prev.messages,
+                    subscriptionData.data.photoAdded,
+                  ],
+                };
+              },
+            })}
+          />
+        );
+      }
+    }
+  </Query>
+);
 
 export default AllPhotos;
