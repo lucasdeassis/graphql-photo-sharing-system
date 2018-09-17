6677c25b5fbf0c4bf71e210f6ddf19fac5bc8d40
lucasdeassis <lucasassis413@gmail.com>
2018-09-17 13:04:01 -0300

    refactor server subscriptions with context pubsub

--- server/graphql/types/photo/resolver.js
+++ server/graphql/types/photo/resolver.js
@@ -1,7 +1,5 @@
-const { PubSub } = require('apollo-server-express');
 const Photo = require('../../../models/photo');
 
-const pubsub = new PubSub();
 const PHOTO_ADDED = 'PHOTO_ADDED';
 const PHOTO_EDITED = 'PHOTO_EDITED';
 const PHOTO_DELETED = 'PHOTO_DELETED';
@@ -19,38 +17,36 @@ module.exports = {
       photo: (root, { id }, { db, user }) => Photo.query.photo(db, id, user),
     },
     Mutation: {
-      uploadPhoto: async (root, args, { user, db }) => {
-        const newPhoto = await Photo.mutation.uploadPhoto(db, user, args);
+      uploadPhoto: async (root, args, { user, db, pubsub }) => {
+        const photoAdded = await Photo.mutation.uploadPhoto(db, user, args);
 
-        if (newPhoto.id) {
-          pubsub.publish(PHOTO_ADDED, newPhoto);
-        } else {
-          throw new Error(`Could not upload photo ${args.image.name} and add to database.`);
+        if (photoAdded._id) {
+          pubsub.publish(PHOTO_ADDED, { photoAdded });
+          return photoAdded;
         }
 
-        return Photo.query.photo(db, newPhoto.id, user);
+        throw new Error(`Could not upload photo ${args.image.name} and add to database.`);
       },
 
-      editPhoto: async (root, args, { user, db }) => {
-        const edited = await Photo.mutation.editPhoto(db, user, args);
+      editPhoto: async (root, args, { user, db, pubsub }) => {
+        const hasEdited = await Photo.mutation.editPhoto(db, user, args);
 
-        if (edited) {
-          const photo = await Photo.query.photo(db, args.id, user);
-          pubsub.publish(PHOTO_EDITED, photo);
+        if (hasEdited) {
+          const photoEdited = await Photo.query.photo(db, args.id, user);
+          pubsub.publish(PHOTO_EDITED, { photoEdited });
 
-          return photo;
+          return photoEdited;
         }
 
         throw new Error(`Photo with id '${args.id}' was not found.`);
       },
 
-      deletePhoto: async (root, args, { user, db }) => {
-        const deleted = await Photo.mutation.deletePhoto(db, user, args);
+      deletePhoto: async (root, args, { user, db, pubsub }) => {
+        const photoDeleted = await Photo.query.photo(db, args.id, user);
+        const hasDeleted = await Photo.mutation.deletePhoto(db, user, args);
 
-        if (deleted) {
-          const photo = await Photo.query.photo(db, args.id, user);
-
-          pubsub.publish(PHOTO_DELETED, photo);
+        if (hasDeleted) {
+          pubsub.publish(PHOTO_DELETED, { photoDeleted });
           return true;
         }
 
@@ -59,13 +55,13 @@ module.exports = {
     },
     Subscription: {
       photoAdded: {
-        subscribe: () => pubsub.asyncIterator(PHOTO_ADDED),
+        subscribe: (root, args, { pubsub }) => pubsub.asyncIterator(PHOTO_ADDED),
       },
       photoEdited: {
-        subscribe: () => pubsub.asyncIterator(PHOTO_EDITED),
+        subscribe: (root, args, { pubsub }) => pubsub.asyncIterator(PHOTO_EDITED),
       },
       photoDeleted: {
-        subscribe: () => pubsub.asyncIterator(PHOTO_DELETED),
+        subscribe: (root, args, { pubsub }) => pubsub.asyncIterator(PHOTO_DELETED),
       },
     },
   },
